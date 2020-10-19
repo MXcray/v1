f8b965539a55466c4d5d701f115561abbc1b3429
MXcray <urazaev.tosha@yandex.ru>
2020-10-19 19:13:15 +0400

    v1

---
+++ app/js/alert.js
@@ -0,0 +1,44 @@
+var button = document.getElementById("semf");
+button.addEventListener('click', function() {
+	Swal.fire({
+		title: 'Semf Ucuk',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, tempora.',
+		imageUrl: '../images/src/team-avatar-popup.png',
+		imageWidth: 220,
+		imageHeight: 229,
+		imageAlt: 'Custom image',
+	  })
+});
+var button = document.getElementById("dik");
+button.addEventListener('click', function() {
+	Swal.fire({
+		title: 'Dik Adalin',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, tempora.',
+		imageUrl: '../images/src/team-avatar-popup.png',
+		imageWidth: 220,
+		imageHeight: 229,
+		imageAlt: 'Custom image',
+	  })
+});
+var button = document.getElementById("jeng");
+button.addEventListener('click', function() {
+	Swal.fire({
+		title: 'Jeng Kol',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, tempora.',
+		imageUrl: '../images/src/team-avatar-popup.png',
+		imageWidth: 220,
+		imageHeight: 229,
+		imageAlt: 'Custom image',
+	  })
+});
+var button = document.getElementById("pet");
+button.addEventListener('click', function() {
+	Swal.fire({
+		title: 'Pet Romak',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, tempora.',
+		imageUrl: '../images/src/team-avatar-popup.png',
+		imageWidth: 220,
+		imageHeight: 229,
+		imageAlt: 'Custom image',
+	  })
+});
